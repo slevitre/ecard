8991d2390f7849146a9afa95a642a4acb6f8365c
ja-godfrey <ja.godfrey.son@gmail.com>
2023-09-04 14:23:50 -0400

    version 1 - init

--- js/calculator.js
+++
@@ -1,9 +0,0 @@
-function calculateBayesian() {
-    var prior = parseFloat(document.getElementById("prior").value);
-    var likelihood = parseFloat(document.getElementById("likelihood").value);
-    var marginal = parseFloat(document.getElementById("marginal").value);
-  
-    var posterior = (prior * likelihood) / marginal;
-  
-    document.getElementById("result").innerText = posterior;
-  }
